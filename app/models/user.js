6451d46e10df6bba4b4a12d69b1f9879199d6afd
Craig_Donavin <crdonavin@gmail.com>
2020-12-05 21:50:44 -0600

    add bcrypt to user model

--- app/models/user.js
+++ app/models/user.js
@@ -19,6 +19,25 @@ const User = bookshelf.Model.extend({
   comments: function() {
     return this.hasMany(Comments);
   },
+  encryptPassword: (model, attrs, options) => {
+    return new Promise((resolve, reject) => {
+      bcrypt.hash(model.attributes.password, 10, (err, hash) => {
+        if (err) return reject(err);
+        model.set('password', hash);
+        resolve(hash);
+      });
+    });
+  },
+  validatePassword: function(suppliedPassword){
+    let self = this;
+    return new Promise(function(resolve, reject) {
+      const hash = self.attributes.password;
+      bcrypt.compare(suppliedPassword, hash, (err, res) => {
+        if (err) return reject(err);
+        return resolve(res);
+      });
+    });
+  }
 });
 
 module.exports = bookshelf.model('User', User);
